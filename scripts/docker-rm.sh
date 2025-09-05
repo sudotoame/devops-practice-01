4275d7d18b9f0bbe3a831af3999c830a5569f415
yukitoame <admin@localhost.localdomain>
2025-09-06 01:08:34 +0900

    .gitignore added

---
+++ scripts/docker-rm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-firefox:4.35.0-20250808
+docker run -d -p 4444:4444 -p 7900:7900 --name selenium --shm-size="2g" --restart always selenium/standalone-chromium:124.0
