e67026f5871e522d8673c9a714379226878ad576
yukitoame <akaisuki2@gmail.com>
2025-09-06 13:08:11 +0200

    delete demonize from run

--- scripts/docker-run-pytest.sh
+++ scripts/docker-run-pytest.sh
@@ -6,4 +6,4 @@ IMAGE_TAG="$GITHUB_RUN_NUMBER"
 
 docker network inspect $NETWORK_NAME || docker network create $NETWORK_NAME
 
-docker run -d --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
+docker run --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
