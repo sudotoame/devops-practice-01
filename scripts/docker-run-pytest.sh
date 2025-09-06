5f5e06073f831f39fd6f4dcf71b338b526b73e7b
yukitoame <akaisuki2@gmail.com>
2025-09-06 10:53:00 +0200

    changed run script

--- scripts/docker-run-pytest.sh
+++ scripts/docker-run-pytest.sh
@@ -5,4 +5,4 @@ NETWORK_NAME='selenium-network'
 
 docker network inspect $NETWORK_NAME || docker network create $NETWORK_NAME
 
-docker run -d --name pytest --network selenium-network --restart always pytest:1.0
+docker run -d --name pytest --rm --network selenium-network pytest:1.0
