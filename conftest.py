23774a00523f77f23227472da34191cd4b1095ba
yukitoame <akaisuki2@gmail.com>
2025-09-06 13:36:59 +0200

    conftest.py changed

--- conftest.py
+++ conftest.py
@@ -16,6 +16,6 @@ def driver():
         command_executor="http://selenium:4444/wd/hub", desired_capabilities = desiredCapabilities)
  #   driver.maximize_window()
     yield driver
-    attach = driver.get_screenshot_as_png()
-    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
+ #   attach = driver.get_screenshot_as_png()
+ #   allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
