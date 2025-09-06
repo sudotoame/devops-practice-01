7c795d41fa3dbe46b5386823dc7f343a72f849cc
yukitoame <akaisuki2@gmail.com>
2025-09-06 12:19:22 +0200

    workflow fixed

--- scripts/docker-run-pytest.sh
+++ scripts/docker-run-pytest.sh
@@ -2,7 +2,8 @@
 
 set -e
 NETWORK_NAME='selenium-network'
+IMAGE_TAG='GITHUB_RUN_NUMBER'
 
 docker network inspect $NETWORK_NAME || docker network create $NETWORK_NAME
 
-docker run -d --name pytest --rm --network selenium-network pytest:1.0
+docker run -d --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
