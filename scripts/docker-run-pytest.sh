5e80cc9f1d2a24aad481b164478ed7fac6c8048b
yukitoame <akaisuki2@gmail.com>
2025-09-08 11:35:29 +0200

    add resources for container

--- scripts/docker-run-pytest.sh
+++ scripts/docker-run-pytest.sh
@@ -6,4 +6,4 @@ IMAGE_TAG="$GITHUB_RUN_NUMBER"
 
 docker network inspect $NETWORK_NAME || docker network create $NETWORK_NAME
 
-docker run --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
+docker run --cpus 2 --memory 1G --memory-swap 2G --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
