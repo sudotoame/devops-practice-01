2df5aecff364ab1e0849f768b4cb46751ad3af68
yukitoame <akaisuki2@gmail.com>
2025-09-08 12:35:52 +0200

    add voluem for container

--- scripts/docker-run-pytest.sh
+++ scripts/docker-run-pytest.sh
@@ -6,4 +6,12 @@ IMAGE_TAG="$GITHUB_RUN_NUMBER"
 
 docker network inspect $NETWORK_NAME || docker network create $NETWORK_NAME
 
-docker run --cpus 1 --memory 1G --memory-swap 2G --name pytest --rm --network selenium-network pytest:${IMAGE_TAG}
+docker run \
+       	--cpus 1 \
+       	--memory 1G \
+       	--memory-swap 2G \
+       	--name pytest \
+       	--rm \
+       	--network selenium-network \
+       	pytest:${IMAGE_TAG} \
+	-v /data/allure-results:/app/allure-results
